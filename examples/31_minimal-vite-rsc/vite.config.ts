3b00b42108025aebd20ca26facfdf077e6232a39
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-20 18:41:03 +0900

    chore: cleanup

--- examples/31_minimal-vite-rsc/vite.config.ts
+++ examples/31_minimal-vite-rsc/vite.config.ts
@@ -1,10 +1,11 @@
-import { defineConfig } from 'vite';
+import { defineConfig, Plugin } from 'vite';
 import rsc from '@hiogawa/vite-rsc/plugin';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
   plugins: [
     react(),
+    rscWaku(),
     rsc({
       entries: {
         client: './src/client-entry.tsx',
@@ -12,9 +13,26 @@ export default defineConfig({
         rsc: './framework/entry.rsc.tsx',
       },
     }),
+  ],
+});
+
+function rscWaku(): Plugin[] {
+  return [
+    {
+      name: 'rsc:waku',
+      config() {
+        return {
+          define: {
+            'import.meta.env.WAKU_CONFIG_BASE_PATH': JSON.stringify(''),
+            'import.meta.env.WAKU_CONFIG_RSC_BASE': JSON.stringify(''),
+          },
+          environments: {},
+        };
+      },
+    },
     {
       // rewrite `react-server-dom-webpack` in `waku/minimal/client`
-      name: 'waku-wip',
+      name: 'rsc:waku:patch-webpack',
       enforce: 'pre',
       resolveId(source, importer, options) {
         if (source === 'react-server-dom-webpack/client') {
@@ -33,9 +51,5 @@ export default defineConfig({
         }
       },
     },
-  ],
-  define: {
-    'import.meta.env.WAKU_CONFIG_BASE_PATH': JSON.stringify(''),
-    'import.meta.env.WAKU_CONFIG_RSC_BASE': JSON.stringify(''),
-  },
-});
+  ];
+}
