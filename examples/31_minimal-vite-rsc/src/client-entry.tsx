190c52c70519bb2afe337158094308e49d5bd5f8
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-21 13:08:54 +0900

    chore: cleanup

--- examples/31_minimal-vite-rsc/src/client-entry.tsx
+++ examples/31_minimal-vite-rsc/src/client-entry.tsx
@@ -1,9 +1,6 @@
 import { StrictMode } from 'react';
 import { createRoot, hydrateRoot } from 'react-dom/client';
-import { Root, Slot, unstable_callServerRsc } from 'waku/minimal/client';
-import * as ReactClient from '@hiogawa/vite-rsc/browser';
-
-ReactClient.setServerCallback(unstable_callServerRsc);
+import { Root, Slot } from 'waku/minimal/client';
 
 const rootElement = (
   <StrictMode>
@@ -17,10 +14,4 @@ if ((globalThis as any).__WAKU_HYDRATE__) {
   hydrateRoot(document, rootElement);
 } else {
   createRoot(document as any).render(rootElement);
-}
-
-if (import.meta.hot) {
-  import.meta.hot.on('rsc:update', () => {
-    // TODO
-  });
 }
