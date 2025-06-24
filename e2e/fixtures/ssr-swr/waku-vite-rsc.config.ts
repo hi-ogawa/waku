529cea0163921a06c0e28bda56dda8ac987dd071
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-24 18:52:01 +0900

    test: ssr-swr

---
+++ e2e/fixtures/ssr-swr/waku-vite-rsc.config.ts
@@ -0,0 +1,17 @@
+import waku from 'waku/vite-rsc/plugin';
+
+export default {
+  plugins: [waku()],
+  environments: {
+    client: {
+      optimizeDeps: {
+        include: ["swr"]
+      }
+    },
+    ssr: {
+      optimizeDeps: {
+        include: ["swr"]
+      }
+    },
+  }
+}
