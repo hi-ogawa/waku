2cb734b0c26516c1014e63fb752a3544fcfe1e04
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-26 16:30:20 +0900

    refactor: simplify entry.vercel

--- packages/waku/src/vite-rsc/deploy/vercel/entry.vercel.tsx
+++ packages/waku/src/vite-rsc/deploy/vercel/entry.vercel.tsx
@@ -1,7 +1,4 @@
-import { Hono } from 'hono';
-import handler from '../../entry.rsc.js';
+import { app } from '../../entry.rsc.default.js';
 import { getRequestListener } from '@hono/node-server';
 
-const app = new Hono();
-app.use((ctx) => handler(ctx.req.raw));
 export default getRequestListener(app.fetch);
