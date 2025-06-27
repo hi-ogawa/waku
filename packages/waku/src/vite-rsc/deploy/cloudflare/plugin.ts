bdf43d84d52a394f25362ba9fc33b7f07ec7b103
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-06-27 15:39:13 +0900

    feat: cloudflare

--- packages/waku/src/vite-rsc/deploy/cloudflare/plugin.ts
+++ packages/waku/src/vite-rsc/deploy/cloudflare/plugin.ts
@@ -1,5 +1,10 @@
-import { type Plugin } from 'vite';
+import { type EnvironmentOptions, type Plugin } from 'vite';
 import type { Config } from '../../../config.js';
+import { separatePublicAssetsFromFunctions } from '../../../lib/plugins/vite-plugin-deploy-cloudflare.js';
+import path from 'node:path';
+import { existsSync, writeFileSync } from 'node:fs';
+
+const SERVE_JS = 'serve-cloudflare.js';
 
 export function wakuDeployCloudflarePlugin(deployOptions: {
   wakuConfig: Required<Config>;
@@ -7,6 +12,20 @@ export function wakuDeployCloudflarePlugin(deployOptions: {
   return {
     name: 'waku:deploy-cloudflare',
     config() {
+      // configures environment like @cloudflare/vite-plugin
+      // https://github.com/cloudflare/workers-sdk/blob/869b7551d719ccfe3843c25e9907b74024458561/packages/vite-plugin-cloudflare/src/cloudflare-environment.ts#L131
+      const serverOptions: EnvironmentOptions = {
+        resolve: {
+          conditions: [
+            'workerd',
+            'module',
+            'browser',
+            'development|production',
+          ],
+        },
+        keepProcessEnv: false,
+      };
+
       return {
         environments: {
           rsc: {
@@ -17,7 +36,9 @@ export function wakuDeployCloudflarePlugin(deployOptions: {
                 },
               },
             },
+            ...serverOptions,
           },
+          ssr: serverOptions,
         },
       };
     },
@@ -29,8 +50,54 @@ export function wakuDeployCloudflarePlugin(deployOptions: {
           return;
         }
         const config = this.environment.getTopLevelConfig();
-        config.root;
-        deployOptions.wakuConfig;
+        const rootDir = config.root;
+        const opts = deployOptions.wakuConfig;
+
+        const outDir = path.join(rootDir, opts.distDir);
+        const assetsDistDir = path.join(outDir, 'assets');
+        const workerDistDir = path.join(outDir, 'worker');
+
+        writeFileSync(
+          path.join(outDir, SERVE_JS),
+          `export { default } from './rsc/index.js';\n`,
+        );
+
+        separatePublicAssetsFromFunctions({
+          outDir,
+          assetsDir: assetsDistDir,
+          functionDir: workerDistDir,
+        });
+
+        const wranglerTomlFile = path.join(rootDir, 'wrangler.toml');
+        const wranglerJsonFile = path.join(rootDir, 'wrangler.json');
+        const wranglerJsoncFile = path.join(rootDir, 'wrangler.jsonc');
+        if (
+          !existsSync(wranglerTomlFile) &&
+          !existsSync(wranglerJsonFile) &&
+          !existsSync(wranglerJsoncFile)
+        ) {
+          writeFileSync(
+            wranglerJsoncFile,
+            `\
+{
+  "name": "waku-project",
+  "main": "./dist/worker/serve-cloudflare.js",
+  // https://developers.cloudflare.com/workers/platform/compatibility-dates
+  "compatibility_date": "2024-11-11",
+  // nodejs_als is required for Waku server-side request context
+  // It can be removed if only building static pages
+  "compatibility_flags": ["nodejs_als"],
+  // https://developers.cloudflare.com/workers/static-assets/binding/
+  "assets": {
+    "binding": "ASSETS",
+    "directory": "./dist/assets",
+    "html_handling": "drop-trailing-slash",
+    "not_found_handling": "404-page"
+  }
+}
+`,
+          );
+        }
       },
     },
   };
