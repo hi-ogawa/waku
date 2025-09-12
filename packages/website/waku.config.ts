418752fbb0f8569cfa315fedc0550a97915e9814
Hiroshi Ogawa <hi.ogawa.zz@gmail.com>
2025-09-12 10:20:20 +0900

    fix: workaround double flash on dev

--- packages/website/waku.config.ts
+++ packages/website/waku.config.ts
@@ -3,7 +3,22 @@ import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   vite: {
-    plugins: [tailwindcss()],
+    plugins: [
+      tailwindcss(),
+      {
+        // https://github.com/hi-ogawa/reproductions/tree/main/vite-rsc-fontsource
+        name: 'fix-dev-font-double-flash',
+        configureServer(server) {
+          server.middlewares.use((req, res, next) => {
+            const url = new URL(req.url || '', 'http://localhost');
+            if (url.pathname.endsWith('.woff2')) {
+              res.setHeader('cache-control', 'max-age=10');
+            }
+            next();
+          });
+        },
+      },
+    ],
     environments: {
       rsc: {
         resolve: {
